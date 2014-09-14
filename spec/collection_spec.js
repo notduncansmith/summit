9aae3ba9ad8723fd586183477f39945cec925532
Duncan Smith <hello@duncanmsmith.com>
2014-09-13 20:40:36 -0500

    Stub out Collection interface

--- spec/collection_spec.js
+++ spec/collection_spec.js
@@ -1,27 +1,69 @@
-var db = {}
-  , CMS = require('../lib/cms')
-  , Cms = new CMS({});
+var Collection = require('../lib/collection')
+  , Promise = require('bluebird')
+  , fakeDb = {
+    use: function () {return fakeDb},
+    get: function () {return new Promise.resolve(null)},
+    put: function () {return new Promise.resolve(null)}
+  }
+  , Coll = new Collection({
+    name: 'Foo',
+    fields: {
+      title: 'string',
+      body: 'text',
+      md: 'markdown',
+      phone: 'phone',
+      email: 'email',
+      num: 'number',
+      cb: 'bool'
+    }
+  }, fakeDb);
 
-describe('The CMS module', function () {
-  
-  describe('Has a method called collection, which', function () {
-    var doc = {name:'foo', foo: 'bar'}
-      , docName = 'foo';
+describe('A Collection', function () {
+
+  describe('has a method called form, which', function () {
+    var form = null;
 
     beforeEach(function () {
-      spyOn(Cms.collections, 'get');
-      spyOn(Cms.collections, 'put');
-
-      Cms.collection(docName);
-      Cms.collection(doc);
+      form = Coll.form();
+      console.log(form)
     });
 
-    it('when passed a string, should get a collection by name', function () {
-      expect(Cms.collections.get).toHaveBeenCalledWith(docName);
+    it('returns a string', function () {
+      expect(form).toEqual(jasmine.any(String));
+    });
+  });
+
+  describe('has a method called setup, which', function () {
+
+    it('checks to see if the design doc exists', function (done) {
+      spyOn(fakeDb, 'get').andCallThrough();
+
+      Coll.setup()
+      .then(function () {
+       expect(fakeDb.get).toHaveBeenCalledWith('_design/cms-Foo');
+       done();
+      });
     });
 
-    it('when passed an object, should create a collection with the given name', function () {
-      expect(Cms.collections.put).toHaveBeenCalledWith(docName, doc);
+    it('creates the database if the design doc does not exist', function (done) {
+      var newDoc = {
+        _id: "_design/cms-Foo",
+        views: {
+          all: "function (doc) { if (doc.collection == 'Foo') { emit(doc.id, doc); } }"
+        }
+      };
+
+      spyOn(fakeDb, 'get').andCallFake(function () {
+        return new Promise.reject(null);
+      });
+
+      spyOn(fakeDb, 'put').andCallFake(console.log);
+
+      Coll.setup()
+      .then(function () {
+       expect(fakeDb.put).toHaveBeenCalledWith('_design/cms-Foo', JSON.stringify(newDoc));
+       done();
+      });
     });
   });
 });
