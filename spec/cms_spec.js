19b649a912f22d81107f9c8345dbafcbab168079
Duncan Smith <hello@duncanmsmith.com>
2014-09-13 20:38:51 -0500

    Collections are stored in memory
    Having collections stored in the database was a stupid idea.  They are now just stored in memory as part of the Cms object.

---
+++ spec/cms_spec.js
@@ -0,0 +1,25 @@
+var db = {}
+  , CMS = require('../lib/cms')
+  , Cms = new CMS({});
+
+describe('The CMS module', function () {
+  
+  describe('Has a method called collection, which', function () {
+    var doc = {name:'foo', foo: 'bar'}
+      , docName = 'foo'
+      , result = null;
+
+    beforeEach(function () {
+      Cms.collection(doc);
+      result = Cms.collection(docName);
+    });
+
+    it('when passed an object, should create a collection with the given name', function () {
+      expect(Cms.collections.foo.name).toEqual('foo');
+    });
+
+    it('when passed a string, should get a collection by name', function () {
+      expect(result.name).toEqual('foo');
+    });
+  });
+});
