b66015fbfd4c003c31d60b314ae8162b5b236dff
Duncan Smith <hello@duncanmsmith.com>
2014-09-14 17:57:06 -0500

    Update CMS spec to match refactor

--- spec/cms_spec.js
+++ spec/cms_spec.js
@@ -15,11 +15,11 @@ describe('The CMS module', function () {
     });
 
     it('when passed an object, should create a collection with the given name', function () {
-      expect(Cms.collections.foo.name).toEqual('foo');
+      expect(Cms.collections.foo.collection.name).toEqual('foo');
     });
 
     it('when passed a string, should get a collection by name', function () {
-      expect(result.name).toEqual('foo');
+      expect(result.collection.name).toEqual('foo');
     });
   });
 });
