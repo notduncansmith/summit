08861ce6382b0d67bebf4d457e304a293b70b1b8
Duncan Smith <hello@duncanmsmith.com>
2014-09-13 20:52:11 -0500

    App returns CMS class
    No need to mess with all the other junk since we're in library mode for now.

--- app.js
+++ app.js
@@ -1,33 +1,5 @@
-var bogart = require('bogart-edge')
-  , path = require('path')
-  , config = require('./config')
-  , Database = require('./lib/db')
-  , sessionSecret = config.environment.sessionSecret
-  , assetPath = path.join(bogart.maindir(), 'assets')
-  , router = require('./lib/router')
-  , handlebars = require('./lib/middleware/bogart_handlebars')();
+var CMS = require('./lib/cms');
 
-function createApp () {
-  var app = bogart.app();
-  app.injector.value('env', process.env);
-  
-  app.use(bogart.middleware.session({
-    secret: sessionSecret
-  }));
-  
-  app.use(function(req, injector, next) {
-    app.injector.value('session', req.session);
-    app.injector.value('flash', req.flash);
-    return next();
-  });
-  
-  app.use(bogart.middleware.error());
-  app.use(bogart.middleware.parted());
-  app.use(handlebars);
-  app.use(router());
-  app.use(bogart.middleware.directory(assetPath));
-
-  return app;
-};
-
-createApp().start(config.environment.port);
+module.exports = function (config) {
+  return new CMS(config);
+}
