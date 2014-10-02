fdfba31846f08118a785e0c42696c8c3d074a16a
Duncan Smith <hello@duncanmsmith.com>
2014-10-01 21:08:35 -0500

    Export UUID module

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ CMS.bogart = require('bogart-edge');
 CMS._ = require('lodash');
 CMS.inflection = require('inflection');
 CMS.Promise = require('bluebird');
+CMS.uuid = require('node-uuid').v4;
 
 module.exports = CMS;
