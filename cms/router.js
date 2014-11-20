5d051b55b13f2311eb5985d8be0a8796fa28a75b
Duncan Smith <hello@duncanmsmith.com>
2014-11-20 10:32:30 -0600

    Updated CMS to not break + added by default to server.js

--- cms/router.js
+++ cms/router.js
@@ -3,21 +3,21 @@ var Summit = require('../app');
 module.exports = function (app) {
   var router = app.router();
 
-  router.get('/new-post', function (req, params, CMS) {
+  router.get('/new-post', function (req, Post) {
     var post = {
       title: req.params.title
     };
 
-    return CMS.put(post)
+    return Post.put(post)
     .then(function (result) {
-      return Summit.json(results);
+      return Summit.json(result);
     });
   });
 
-  router.get('/posts', function (req, views, respond, CMS) {
-    return CMS.Post.all()
+  router.get('/posts', function (req, views, respond, Post) {
+    return Post.all()
     .then(function (posts) {
-      return respond(views.cms.post, {posts: posts});
+      return respond(views.post, {posts: posts});
     });
   });
 
