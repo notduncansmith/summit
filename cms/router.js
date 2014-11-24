8c0fe0cba544cdd89c8a9c869143acd80313f754
Martin Murphy <martin.murphy@whiteboard-it.com>
2014-11-23 23:39:50 -0600

    Added routes for editing and saving pages.

--- cms/router.js
+++ cms/router.js
@@ -13,11 +13,58 @@ module.exports = function (app) {
       return Summit.json(result);
     });
   });
+  router.get('/page', function (req, views, respond, Page){
+    return respond(views.page, {page:{}});
+  })
+  router.get('/page/:id', function (req, Page, views, respond){
+    return Page.get(req.params.id).then(function(page){
+      return respond(views.page, {page:page});
+    })
+  })
 
+  var savePage = function (req, uuid, _, Page){
+    return Page.get(req.params.id).then(function(page){
+      page = page._id ? page : {_id: uuid()};
+      page = _.extend(page, req.params);
+      return Page.put(page).then(function(result){
+        return Summit.redirect('/pages')
+      })   
+    })
+  }
+
+  router.post('/page', savePage)
+  router.post('/page/:id', savePage)
+
+
+  router.get('/pages', function (Page, respond, views){
+    return Page.all().then(function(result){
+      return respond(views.pages, {pages:result})
+    })
+  })
+
+  var savePost = function (req, uuid, _, Post){
+    return Post.get(req.params.id).then(function(post){
+      post = post._id ? post : {_id: uuid()};
+      post = _.extend(post, req.params);
+      return Post.put(post).then(function(result){
+        return Summit.redirect('/posts')
+      })   
+    })
+  }
+  router.post('/post', savePost);
+  router.post('/post/:id', savePost);
+  router.get('/post/:id', function (req, Post, views, respond){
+    return Post.get(req.params.id).then(function(post){
+      return respond(views.post, {post:post})
+    })
+  })
+  router.get('/post', function (req, views, respond){
+    return respond(views.post, {post:{}})
+  })
   router.get('/posts', function (req, views, respond, Post) {
     return Post.all()
     .then(function (posts) {
-      return respond(views.post, {posts: posts});
+      return respond(views.posts, {posts: posts});
     });
   });
 
