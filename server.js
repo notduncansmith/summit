722bd3852161a2c5278778dbb6e667ae22417309
Duncan Smith <hello@duncanmsmith.com>
2014-12-22 13:44:50 -0600

    Multitenant WIP

--- server.js
+++ server.js
@@ -3,14 +3,27 @@ var Summit = require('./app');
 var app = new Summit();
 var router = app.router();
 
-// var User = app.collection({
-//   name: 'User',
-//   restful: true,
-//   fields: {}
-// });
+app.multitenant = true;
+app.collections.User.restful = true;
 
 router.get('/', function () {
   return Summit.text('Hello, world!');
+});
+
+router.get('/user', function (req, User) {
+  return User.findByEmail(req.params.email)
+  .then(Summit.json);
+});
+
+router.get('/user/new', function (User, uuid) {
+  return User.register({
+    email: uuid(),
+    password: uuid(),
+    firstName: 'Joe',
+    lastName: 'Schmoe',
+    username: uuid()
+  })
+  .then(Summit.json);
 });
 
 // router.get('/user/new', function (User, respond, views) {
